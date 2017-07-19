d0f01fbec72667a670cc9af87eaf0410c75abc7d
Octaviowoods <octavio_roh@outlook.com>
2017-07-19 18:46:22 -0500

    Add files via upload

---
+++ shino.R
@@ -0,0 +1,15 @@
+add2<-function(x,y){
+	x+y}
+
+above<-function(x,n){
+	use<-x>n
+	x[use]
+}
+
+meanscolum<-function(x){
+		nc<-ncol(x)
+		prom<-c()
+	for(i in 0:nc){ 
+		prom[i]<-mean(x[,i])}
+print(prom)
+	}
